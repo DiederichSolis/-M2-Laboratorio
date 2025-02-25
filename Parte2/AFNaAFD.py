5c9caff2499af92244f0745f8651370e0073c7c7
DiederichSolis <sol22952@uvg.edu.gt>
2025-02-24 22:42:05 -0600

    feat: AFNaAFD, minimizador, regexAFDS

---
+++ Parte2/AFNaAFD.py
@@ -0,0 +1,93 @@
+from regexaAFD import toAFN
+from pprint import pp
+
+type AFDState = frozenset[int]
+type AFDTransitions = dict[AFDState, dict[str, AFDState]]
+
+
+def fromAFNToAFD(afn):
+    afdTransitions: AFDTransitions = {}
+    closure, inputs = findClosure(afn, 0, set(()))
+    travelAFN(afn, frozenset(closure), frozenset(inputs), afdTransitions)
+
+    accepted = []
+    for state in afdTransitions:
+        if afn["aceptado"] in state:
+            accepted.append(state)
+
+    return newAFD(afdTransitions, accepted)
+
+
+def travelAFN(
+    afn: dict,
+    closure: frozenset,
+    inputs: frozenset,
+    stateTransitions: AFDTransitions,
+):  
+
+    if closure in stateTransitions:
+        return
+
+    stateTransitions[closure] = {}
+
+    for i in inputs:
+        if i == "_":
+            continue
+        reach = set(())
+        reachInputs = set(())
+        for stateIdx in closure:
+            state = afn["transiciones"][stateIdx]
+            if i not in state:
+                continue
+            for targetIdx in state[i]:
+                target, targetInput = findClosure(afn, targetIdx, set(()))
+                reach |= target
+                reachInputs |= targetInput
+
+        reach = frozenset(reach)
+        stateTransitions[closure][i] = reach
+        travelAFN(afn, reach, frozenset(reachInputs), stateTransitions)
+
+
+def findClosure(afn: dict, stateIdx: int, alreadyEvaluated: set) -> tuple[set, set]:
+
+
+    if stateIdx in alreadyEvaluated:
+        return (set(), set())  
+
+    alreadyEvaluated.add(stateIdx)
+
+    original = afn["transiciones"][stateIdx]
+    closure = set([stateIdx])  
+    inputs = set(original.keys()) 
+
+    if "_" in original:
+        for state in original["_"]:
+            foundClosure, foundInputs = findClosure(afn, state, alreadyEvaluated)
+            closure |= set(foundClosure) 
+            inputs |= set(foundInputs)
+
+    return (closure, inputs)
+
+
+
+def initializeOrAppend():
+    pass
+
+
+def newAFD(transitions: AFDTransitions, accepted: list[AFDState]):
+
+    return {"transiciones": transitions, "aceptado": accepted}
+
+
+if __name__ == "__main__":
+    postfix = "a*"
+    print("RegExp:", postfix)
+
+    afn = toAFN(postfix)
+    print("AFN ")
+    pp(afn)
+
+    afd = fromAFNToAFD(afn)
+    print("AFD")
+    pp(afd)
---
+++ Parte2/minimizador.py
@@ -0,0 +1,34 @@
+
+def minimize_afd(afd):
+    accepted_states = set(afd['accepted'])
+    non_accepted_states = set(afd['transitions'].keys()) - accepted_states
+
+    grouped = {}
+    
+    for state, transitions in afd['transitions'].items():
+        is_accepted = state in accepted_states
+        key = (frozenset(transitions.items()), is_accepted)  
+        grouped.setdefault(key, []).append(state)
+
+    minimized_transitions = {}
+    minimized_accepted = set()
+
+   
+    for group in grouped.values():
+        representative = frozenset().union(*group)  
+        minimized_transitions[representative] = {}
+
+        for input_char, next_state in afd['transitions'][group[0]].items():
+            for next_group in grouped.values():
+                if next_state in next_group:
+                    next_representative = frozenset().union(*next_group) 
+                    minimized_transitions[representative][input_char] = next_representative
+                    break
+
+        if any(state in accepted_states for state in group):
+            minimized_accepted.add(representative)
+
+    return {
+        'transiciones': minimized_transitions,
+        'aceptado': list(minimized_accepted)
+    }
