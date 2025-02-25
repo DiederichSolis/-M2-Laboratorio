68e08b6b1aa84e65f1e789c4bd6e64c3d151b4e4
DiederichSolis <sol22952@uvg.edu.gt>
2025-02-24 20:19:34 -0600

    feat: main

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from regexparser import to_postfix
+from arbol import build_syntax_tree, generate_ast_graph
+from afdgenerador import compute_followpos, generate_afd
+from afdminimizador import minimize_afd
+from verificador import validate_string
+
+print("¡Generador de AFD desde una expresión regular 👨🏻‍💻")
+
+while True:
+    regex = input("\nIngrese la expresión regular, si desea finalizar ingrese salir: ")
+    if regex.lower() == 'salir':
+        print("¡Generador de AFD! ")
+        break
+
+    # Convertir regex a postfix y construir árbol sintáctico
+    postfix = to_postfix(regex + "#")
+    root, positions = build_syntax_tree(postfix)
+    followpos = compute_followpos(root, positions)
+
+    # Crear carpeta específica para la regex
+    regex_folder = f"regex-images-afddirect/{regex}"
+    os.makedirs(regex_folder, exist_ok=True)
+
+    # Generar AST y guardarlo
+    generate_ast_graph(root).render(f"{regex_folder}/ast", format="png", cleanup=True)
+
+    # Generar AFD y guardarlo
+    afd, afd_dict = generate_afd(root, positions, followpos)
+    afd.render(f"{regex_folder}/afd", format="png", cleanup=True)
+
+    # Minimizar el AFD y guardarlo
+    minimized_afd = minimize_afd(afd_dict)
+    minimized_afd.render(f"{regex_folder}/afd_minimized", format="png", cleanup=True)
+
+    print(f"\n🔹 Diagramas generados en: {regex_folder}")
+    
+    # Comenzar a validar cadenas hasta que el usuario quiera salir
+    while True:
+        string = input("\nIngrese una cadena para verificar (si desea cambiar de regex escriba cambio: ")
+        if string.lower() == 'cambio':
+            break
+        
+        if validate_string(afd_dict, string):
+            print(f"La cadena '{string}' ES ACEPTADA por la expresión regular.")
+        else:
+            print(f"La cadena '{string}' FUE RECHAZADA por la expresión regular.")
+
+print("\nFINALIZACION")
