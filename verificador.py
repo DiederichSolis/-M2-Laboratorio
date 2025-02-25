0a9b557fdf34634e834b5993cf578e077a91c5fb
DiederichSolis <sol22952@uvg.edu.gt>
2025-02-24 20:14:36 -0600

    feat: verificador y preprocesadorggex

---
+++ verificador.py
@@ -0,0 +1,11 @@
+def validate_string(afd_dict, string):
+   
+    current_state = afd_dict['initial']
+
+    for char in string:
+        if char in afd_dict['transitions'].get(current_state, {}):
+            current_state = afd_dict['transitions'][current_state][char]
+        else:
+            return False  # No hay transición para este carácter
+
+    return current_state in afd_dict['accepted']
